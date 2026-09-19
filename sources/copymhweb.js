47f49a5e0ad33fbd2c57d16a8cb3b14c2fea5729
xyrlsz <xyrlsz@qq.com>
2026-09-20 04:20:32 +0800

    ✨ feat: 更新拷贝漫画和拷贝漫画Web版本至1.0.12，并新增域名支持

--- sources/copymhweb.js
+++ sources/copymhweb.js
@@ -290,7 +290,8 @@ var SOURCE = installSource(new (class extends MangaSource {
             baseUrl: 'https://www.copy4000.com',
             hosts: [
                 'www.mangacopy.com', 'www.copy20.com', 'www.2025copy.com',
-                'www.2026copy.com', 'www.copy3000.com'
+                'www.2026copy.com', 'www.copy3000.com', "www.copy4000.com",
+                "www.copy5000.com"
             ],
             cidRegex: '(?:h5/details/)?comic/([\\w-]+)',
             webConfig: {
